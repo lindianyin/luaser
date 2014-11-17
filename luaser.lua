9ba8ab74320ff80657e26e2d270f01960b81b946
Luwei <luweimy@126.com>
2014-11-17 12:02:43 +0800

    完成序列化

--- luaser.lua
+++ luaser.lua
@@ -1,81 +1,54 @@
--- 这里是一个简单的 Lua table 序列化函数，只支持 number 或 string 做 key ，但是 value 可以是一个 table ，并支持循环引用：
-
-function serialize(t)
-	local mark={}
-	local assign={}
-
-	local function ser_table(tbl,parent)
-		mark[tbl]=parent
-		local tmp={}
-		for k,v in pairs(tbl) do
-			local key= type(k)=="number" and "["..k.."]" or k
-			if type(v)=="table" then
-				local dotkey= parent..(type(k)=="number" and key or "."..key)
-				if mark[v] then
-					table.insert(assign,dotkey.."="..mark[v])
-				else
-					table.insert(tmp, key.."="..ser_table(v,dotkey))
-				end
-			else
-				table.insert(tmp, key.."="..v)
-			end
-		end
-		return "{"..table.concat(tmp,",").."}"
-	end
-
-	return "do local ret="..ser_table(t,"ret")..table.concat(assign," ").." return ret end"
-end
-
-t = { a = 1, b = 2}
-g = { c = 3, d = 4,  t}
-t.rt = g
-
--- print(serialize(t))
-
--- do local ret={a=1,rt={c=3,d=4},b=2}ret.rt[1]=ret return ret end
 local pairs = pairs
 local type = type
 local print = print
+local table = table
+local string = string
 
-local table_ser = function(tablevar)
+local function table_ser(tablevar, parent, mark, assign)
+	-- 标记当前table, 并记录其key名
+	mark[tablevar] = parent
+	-- 记录表中各项
 	local container = {}
 	for k, v in pairs(tablevar) do
-		-- serialize key
-		local keytype = type(k)
+		-- 序列化key
 		local keystr = nil
-		if keytype == "string" then
-			keystr = string.format(".%s", k)
-		elseif keytype == "number" then
+		if type(k) == "string" then 
+			keystr = string.format("[\"%s\"]", k)
+		elseif type(k) == "number" then 
 			keystr = string.format("[%d]", k)
-		elseif keytype == "table" then
-			keystr = string.format("[%s]", table_ser(k))
-		else print("luaser: key type not support")
 		end
-		-- serialize value
-		local valuetype = type(v)
+
+		-- 序列化value
 		local valuestr = nil
-		if valuetype == "table" then
-			valuestr = table_ser(v)
-		elseif valuetype == "function" then
-			valuestr = string.dump(v)
-		else
+		if type(v) == "string" then 
+			valuestr = string.format("\"%s\"", tostring(v))
+		elseif type(v) == "number" then 
 			valuestr = tostring(v)
-			if not valuestr then print("luaser: value type not support")
+		elseif type(v) == "table" then
+			-- 获得从根表到当前表项的完整key， parent(代表父表key)， mark[v]代表table v的key
+			local fullkey = string.format("%s%s", parent, keystr)
+			if mark[v] then table.insert(assign, string.format("%s=%s", fullkey, mark[v]))
+			else valuestr = table_ser(v, fullkey, mark, assign)
 			end
 		end
-		local keyvaluestr = string.format("%s=%s", keystr, valuestr)
-		table.insert(container, keyvaluestr)
+
+		if keystr and valuestr then
+			local keyvaluestr = string.format("%s=%s", keystr, valuestr)
+			table.insert(container, keyvaluestr)
+		end
 	end
 	return string.format("{%s}", table.concat(container, ","))
 end
 
-
-local s= table_ser({
-"nihao",
-fun=function () return "hello" end
-})
-
-print(s)
-function ser(tablevar)
-
+-- string, number, function, table
+local function ser(var, enc)
+	assert(type(var)=="table")
+	-- 标记所有出现的table, 并记录其key, 用于处理循环引用
+	local mark = {}
+	-- 用于记录循环引用的赋值语句
+	local assign = {}
+	-- 序列化表, ret字符串必须与后面的loca ret=%s中的ret相同，因为这个ret可能也会组织到结果字符串中。
+	local ret = table_ser(var, "ret", mark, assign)
+	local ret = string.format("local ret=%s %s return ret", ret, table.concat(assign, ";"))
+	return (enc==nil or enc==true) and string.dump(loadstring(ret)) or ret
 end
